7b80468fb4f463eb6fdb8f0f9cb26301674b0326
Innocent Magagula <innocentm@globalkinetic.com>
2022-09-28 11:44:59 +0200

    feat(kyc): scan sample responses

--- fineract-provider/src/main/java/org/apache/fineract/farmersbank/kyc/api/OrganizationScanApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/farmersbank/kyc/api/OrganizationScanApiResource.java
@@ -37,6 +37,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -44,7 +45,7 @@ import java.io.IOException;
 
 @Component
 @Scope("singleton")
-@Path("/kyc/scan")
+@Path("/screening/organisation")
 @Tag(
         name = "Scan customer PEP & Sanctions status",
         description =
@@ -62,7 +63,7 @@ public class OrganizationScanApiResource {
         this.kycService = kycService;
     }
 
-    @Path("organization")
+    @POST
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     @Operation(
